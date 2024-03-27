10eaff07fb34ebc1880476f9ae7df188a94a79d1
carusina <79801539+carusina@users.noreply.github.com>
2024-03-27 19:39:56 +0900

    Create single_process.c

---
+++ single_process.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <time.h>
+
+int main() {
+    int num = 8000;
+    
+    clock_t start = clock();
+    for(int i = 1; i < num+1; i++) {
+        printf("%d * 7 = %d\n", i, i*7);
+    }
+    clock_t end = clock();
+
+    printf("Processing Time: %lf\n", (double)(end-start));
+}
