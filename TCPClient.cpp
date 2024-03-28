6d67847d901484052a277b56275e866ade9d2dba
carusina <79801539+carusina@users.noreply.github.com>
2024-03-28 18:31:55 +0900

    Update TCPClient.cpp

--- TCPClient.cpp
+++ TCPClient.cpp
@@ -12,16 +12,18 @@ int main(int argc, char *argv[])
 	if (argc > 1) SERVERIP = argv[1];
 
 	// 소켓 생성
-	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0); // 인터넷 주소 체계의 연결형 서비스
+                                                   // 성공적으로 생성시 소켓 디스크립터 반환
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 
 	// connect()
 	struct sockaddr_in serveraddr;
-	memset(&serveraddr, 0, sizeof(serveraddr));
+	memset(&serveraddr, 0, sizeof(serveraddr)); // serveraddr 세팅
 	serveraddr.sin_family = AF_INET;
-	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr);
+	inet_pton(AF_INET, SERVERIP, &serveraddr.sin_addr); // 서버 IP를 AF_INET 기준으로 binary 형태로 변환해서 저장
 	serveraddr.sin_port = htons(SERVERPORT);
-	retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr));
+	retval = connect(sock, (struct sockaddr *)&serveraddr, sizeof(serveraddr)); // 서버에 연결 요청, 바로 수락되지 않고
+                                                                                // 서버의 대기 큐에서 대기 중이면 블로킹 상태
 	if (retval == SOCKET_ERROR) err_quit("connect()");
 
 	// 데이터 통신에 사용할 변수
@@ -43,7 +45,8 @@ int main(int argc, char *argv[])
 			break;
 
 		// 데이터 보내기
-		retval = send(sock, buf, (int)strlen(buf), 0);
+		retval = send(sock, buf, (int)strlen(buf), 0); // 서버에게 buf byte만큼의 buf를 전달
+                                                       // 보낸 데이터의 byte 리턴
 		if (retval == SOCKET_ERROR) {
 			err_display("send()");
 			break;
@@ -51,7 +54,8 @@ int main(int argc, char *argv[])
 		printf("[TCP 클라이언트] %d바이트를 보냈습니다.\n", retval);
 
 		// 데이터 받기
-		retval = recv(sock, buf, retval, MSG_WAITALL);
+		retval = recv(sock, buf, retval, MSG_WAITALL); // 서버로 부터 촤대 retval byte만큼의 데이터를 받아 buf에 저장
+                                                       // 받은 데이터의 byte 리턴
 		if (retval == SOCKET_ERROR) {
 			err_display("recv()");
 			break;
@@ -62,7 +66,7 @@ int main(int argc, char *argv[])
 		// 받은 데이터 출력
 		buf[retval] = '\0';
 		printf("[TCP 클라이언트] %d바이트를 받았습니다.\n", retval);
-		printf("[받은 데이터] \n%s\n", buf);
+		printf("[받은 데이터] %s\n", buf);
 	}
 
 	// 소켓 닫기
