12554753f4caecd71438decc5cb782245b85ca19
carusina <79801539+carusina@users.noreply.github.com>
2024-04-03 19:17:39 +0900

    Update multi_process_thread.c

--- multi_process_thread.c
+++ multi_process_thread.c
@@ -13,7 +13,7 @@ int cnt_p = -1;
 int num = 25;
 int muls[] = {3, 5, 7, 9};
 
-
+// 각 스레드에서 실행할 작업
 void *func(void *input) {
     int cnt = (int)input;
 
@@ -27,15 +27,15 @@ int main() {
     // wait 인자
     int status;
     pid_t pid;
-    pthread_t tid[4][4];
+    pthread_t tid[4][4]; // thread ID 저장할 배열
 
     clock_t start = clock();    // 프로그램 수행 시간 측정
     for(int i = 0; i < N; i++) {
         // 프로세스 생성
         pid = fork();
-        cnt_p++;
+        cnt_p++; // 프로세서 카운트
         
-        int cnt_t = 0;
+        int cnt_t = 0; // 각 프로세서의 스레트 카운트 
         if(pid == 0) {
             printf("%d번째 프로세스 생성\n", cnt_p+1);
             for(int j = 0; j < N; j++) {
@@ -43,6 +43,7 @@ int main() {
                 cnt_t++;
                 printf("%d번째 프로세스의 %d번째 스레드 생성\n", cnt_p+1, cnt_t);
             }
+            // 해당 스레드가 끝날 때 까지 대기
             for(int j = 0; j < N; j++) {
                 pthread_join(tid[i][j], NULL);
             }
