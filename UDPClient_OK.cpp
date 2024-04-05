7d899c3332556cf011b34970f8d3a5152814c548
carusina <79801539+carusina@users.noreply.github.com>
2024-04-05 17:01:17 +0900

    Update UDPClient_OK.cpp

--- UDPClient_OK.cpp
+++ UDPClient_OK.cpp
@@ -10,11 +10,13 @@ typedef struct Server
 	SOCKET sock;
 	struct sockaddr_in serveraddr;
 	struct sockaddr_in peeraddr;
-	socklen_t addrlen;
 };
 
 void *send_msg(void * arg) {
 	Server *server = (Server *)arg;
+	SOCKET sock = server->sock;
+	struct sockaddr_in serveraddr = server->serveraddr;
+	struct sockaddr_in peeraddr = server->peeraddr;
 
 	while(1) {
 		char ch[2];
@@ -27,8 +29,8 @@ void *send_msg(void * arg) {
 		int len = (int)strlen(buf);
 
 		// 데이터 보내기
-		int retval = sendto(server->sock, buf, len, 0,
-			(struct sockaddr *)&server->peeraddr, sizeof(server->peeraddr));
+		int retval = sendto(sock, buf, len, 0,
+			(struct sockaddr *)&peeraddr, sizeof(peeraddr));
 		if (retval == SOCKET_ERROR) {
 			err_display("sendto()");
 			break;
@@ -40,20 +42,23 @@ void *send_msg(void * arg) {
 
 void *recv_msg(void *arg) {
 	Server *server = (Server *)arg;
+	SOCKET sock = server->sock;
+	struct sockaddr_in serveraddr = server->serveraddr;
+	struct sockaddr_in peeraddr = server->peeraddr;
 
 	while(1) {
 		// 데이터 받기
 		buf[0] = '\0';
 
-		server->addrlen = sizeof(server->peeraddr);
-		int retval = recvfrom(server->sock, buf, BUFSIZE, 0,
-			(struct sockaddr *)&server->peeraddr, &server->addrlen);
+		socklen_t addrlen = sizeof(peeraddr);
+		int retval = recvfrom(sock, buf, BUFSIZE, 0,
+			(struct sockaddr *)&peeraddr, &addrlen);
 		if (retval == SOCKET_ERROR) {
 			err_display("recvfrom()");
 			break;
 		}
 
-		if (memcmp(&server->peeraddr, &server->serveraddr, sizeof(server->peeraddr))) { // 데이터를 받은 주소와 서버의 주소를 비교
+		if (memcmp(&peeraddr, &serveraddr, sizeof(peeraddr))) { // 데이터를 받은 주소와 서버의 주소를 비교
 			printf("[오류] 잘못된 데이터입니다!\n");
 			break;
 		}
@@ -81,10 +86,10 @@ int main(int argc, char *argv[])
 	if (sock == INVALID_SOCKET) err_quit("socket()");
 
 	// 소켓 주소 구조체 초기화
-	memset(&server->serveraddr, 0, sizeof(server->serveraddr)); // serveraddr 초기화
-	server->serveraddr.sin_family = AF_INET;
-	inet_pton(AF_INET, SERVERIP, &server->serveraddr.sin_addr); // 서버 IP를 AF_INET 기준으로 binary 형태로 변환해서 저장
-	server->serveraddr.sin_port = htons(SERVERPORT);
+	memset(&server.serveraddr, 0, sizeof(server.serveraddr)); // serveraddr 초기화
+	server.serveraddr.sin_family = AF_INET;
+	inet_pton(AF_INET, SERVERIP, &server.serveraddr.sin_addr); // 서버 IP를 AF_INET 기준으로 binary 형태로 변환해서 저장
+	server.serveraddr.sin_port = htons(SERVERPORT);
 
 	// 클라이언트와 데이터 통신
 	pthread_create(&sendT, NULL, send_msg, (void *)&client);
@@ -94,6 +99,6 @@ int main(int argc, char *argv[])
 	pthread_join(recvT, NULL);
 
 	// 소켓 닫기
-	close(sock);
+	close(server.sock);
 	return 0;
 }
