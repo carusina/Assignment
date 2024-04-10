4b5dea7779c0cdb4c0a1e05e710b01439275beba
carusina <79801539+carusina@users.noreply.github.com>
2024-04-10 18:34:10 +0900

    Update Dijkstra.c

--- Dijkstra.c
+++ Dijkstra.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 #include <pthread.h>
 
-#define NUMTHREAD 4
+#define NUMTHREAD 4 // 스레드 개수
 
 int flag[NUMTHREAD] = {-1, -1, -1, -1}; // -1:idle / 0:wait-in / 1:in-CS 
-int turn = 0;
+int turn = 0; // 어떤 스레드의 턴인지
 int num = 1;
 int max = 100;
 
+// 3의 배수 출력
 void multi(int st) {
     printf("func%d start\n", st);
     for(int i = max/NUMTHREAD; num < max+1 && i > 0; num++, i--) {
         printf("%d * 3 = %d\t", num, num*3);
-        // printf("%d  ", num);
     }
     printf("\nfunc%d end\n", st);
 }
 
 void *func0 (void *arg) {
     while(1) {
-        flag[0] = 0;
-        while(turn != 0) {
-            if(flag[turn] == -1) {
-                turn = 0;
+        flag[0] = 0; // wait-in 상태
+        while(turn != 0) { // 0번 스레드의 턴이 아니면
+            if(flag[turn] == -1) { // 현재 턴인 스레드의 플래그가 idel이면
+                turn = 0; // 0번 스레드의 턴으로 변경
             }
         }
 
-        flag[0] = 1;
+        flag[0] = 1; // in-CS 상태
         int i = 0;
-        while(i < NUMTHREAD && (i == 0 || flag[i] != 1)) {
-            i++;
+        while(i < NUMTHREAD && (i == 0 || flag[i] != 1)) { // in-CS 상태인
+            i++;                                           // 다른 스레드가 있는지 확인
         }
-        if(i >= NUMTHREAD) {
-                break;
+        if(i >= NUMTHREAD) { // in-CS 상태의 스레드가 0번 스레드 밖에 없다면
+                break;       // 임계 영역 진입
         }
     }
-
+    // 임계 영역 시작
     multi(0);
-    flag[0] = -1;
+    // 임계 영역 종료
+    flag[0] = -1; // idle 상태
     pthread_exit(NULL);
 }
 
