1a45f8c9072a19487b3a8de32e2d78b561db4252
carusina <79801539+carusina@users.noreply.github.com>
2024-05-25 01:53:15 +0900

    Create MLFQ.c

---
+++ MLFQ.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+#define ProcessNum 3
+
+int current_time = 0;
+int is_finish = 0;
+int is_timed = 1;
+int is_scheduling = 0;
+int is_in = 0;
+
+int arrive_time[ProcessNum] = {0,1,2};
+int need_time[ProcessNum] = {30,20,10};
+int process_priority[ProcessNum] = {0, 0, 0};
+int RobinTime[3] = {1,2,4};
+
+int running_time[ProcessNum];
+int drop_time[ProcessNum];
+int start_time[ProcessNum];
+int end_time[ProcessNum];
+
+int finish_order[ProcessNum];
+int order = 0;
+char buf[999];
+char gant[999];
+
+struct List {
+    struct List* next;
+    int num;
+    int priority;
+};
+struct List* head = NULL;
+struct List* tail = NULL;
+
+void scheduling(struct List* process, int is_first) {
+    if(is_first == 1) {
+        while(is_scheduling != process->num) {}
+
+        if(head == NULL) {
+            head = process;
+            is_scheduling++;
+            is_in = 0;
+            return;
+        }
+
+        struct List* prev = NULL;
+        struct List* cur = head;
+        while(cur != NULL && process->priority > cur->priority) {
+            prev = cur;
+            cur = cur->next;
+        }
+        if(prev == NULL) {
+            sprintf(buf, "P%d (%d-%d)\n", head->num+1, start_time[head->num], current_time);
+            strcat(gant, buf);
+            // start_time[head->num] = -1;
+
+            process->next = head;
+            head = process;
+            // printf("\n\nclear\n\n");
+        }
+        else {
+            prev->next = process;
+            process->next = cur;
+            // printf("\n\nclear\n\n");
+        }
+        
+        is_scheduling++;
+        is_in = 0;
+    }
+    else {
+        while(is_in == 1) {}
+
+        struct List* temp = NULL;
+        struct List* head_next = process->next;
+
+        int head_change = 0;
+        while(process->next != NULL && process->priority >= process->next->priority) {
+            head_change = 1;
+
+            temp = process->next;
+            process->next = temp->next;
+            temp->next = process;
+        }
+        if(head_change == 1 && head_next != NULL) head = head_next;
+
+        if(process != head) {
+            sprintf(buf, "P%d (%d-%d)\n", head->num+1, start_time[head->num], current_time);
+            strcat(gant, buf);
+            // start_time[process->num] = -1;
+        }
+    }
+}
+
+void* func(void* args) {
+    struct List* input = (struct List*)args;
+    int cnt = 0;
+
+    while(running_time[input->num] < need_time[input->num]) {
+        while(is_finish == 0 && is_timed == 1) {}
+        if(input == head) {
+            if(start_time[input->num] == -1) start_time[input->num] = current_time;
+            printf("P%d: %d X %d = %d\n", input->num+1, running_time[input->num]+1, input->num+1, (running_time[input->num]+1)*(input->num+1));
+            running_time[input->num]++;
+            current_time++;
+            cnt++;
+            is_timed = 1;
+
+            if(running_time[input->num] <= need_time[input->num] && current_time-start_time[input->num] >= RobinTime[input->priority]) {
+                while(is_finish == 0 && is_timed == 1) {}
+
+                if((input == head && input->next == NULL) || need_time[input->num]-running_time[input->num] <= 1) input->priority = 0;
+                else if(cnt == 1 || need_time[input->num]-running_time[input->num] <= 2) input->priority = 1;
+                else input->priority = 2;
+                printf("cnt: %d / needT: %d / runningT: %d / priority: %d\n\n", cnt, need_time[input->num], running_time[input->num], input->priority);
+                scheduling(input, 0);
+            }
+            if(input != head) start_time[input->num] = -1;
+        }
+    }
+    end_time[input->num] = current_time;
+    head = input->next;
+    finish_order[order++] = input->num;
+    sprintf(buf, "P%d (%d-%d)\n", input->num+1, start_time[input->num], end_time[input->num]);
+    strcat(gant, buf);
+
+    free(input);
+    pthread_exit(NULL);
+}
+
+int main() {
+    for(int i = 0; i < ProcessNum; i++) start_time[i] = -1;
+    for(int i = 0; i < ProcessNum; i++) drop_time[i] = -1;
+
+    pthread_t tid[ProcessNum];
+    int num = 0;
+
+    while(current_time <= arrive_time[ProcessNum-1]) {
+        if(current_time == arrive_time[num] && is_timed == 1) {
+            struct List* newp = (struct List*)malloc(sizeof(struct List));
+            newp->priority = 0;
+            newp->next = NULL;
+            newp->num = num;
+            
+            is_in = 1;
+            scheduling(newp, 1);
+            pthread_create(&tid[num], NULL, func, (void*)newp);
+            
+            num++;
+            is_timed = 0;
+        }
+    }
+    is_finish = 1;
+    for(int i = 0; i < num; i++) pthread_join(tid[i], NULL);
+    printf("%s",gant);
+
+    int sum_return_time = 0;
+    int sum_waiting_time = 0;
+    for(int i = 0; i < ProcessNum; i++) {
+        int return_time = end_time[i]-arrive_time[i];
+        int waiting_time = end_time[i]-running_time[i]-arrive_time[i];
+        printf("P%d 반환시간: %2d 대기시간: %2d\n", i+1, return_time, waiting_time);
+        sum_return_time += return_time;
+        sum_waiting_time += waiting_time;
+    }
+    printf("평균 반환시간: %lf 평균 대기시간: %lf\n", (double)sum_return_time/ProcessNum, (double)sum_waiting_time/ProcessNum);
+}
